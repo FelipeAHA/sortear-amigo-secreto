8865679dabc77abfb35ed30f84e146ae01639960
Felipe Azevedo <felipeazevedo137@gmail.com>
2025-01-28 19:55:54 -0300

    Construindo a lógica em JS

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomesAmigos = [];
+
+let adicionar = document.getElementById('amigo');
+adicionar.addEventListener('keypress', function(event){
+    if (event.key == 'Enter'){
+        adicionarAmigo();
+    }
+})
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    if (nome == ''){
+        alert('Por favor, insira um nome.')
+    } else {
+        nomesAmigos.push(nome);
+        percorrerLista();
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    nome = document.getElementById('amigo');
+    nome.value = '';
+}
+
+function percorrerLista(){
+    let contador = nomesAmigos.length -1;
+    let newEl = document.createElement('li');
+    let newText = document.createTextNode(nomesAmigos[contador]);
+    newEl.appendChild(newText);
+    let lista = document.getElementById('listaAmigos');
+    lista.appendChild(newEl);
+}
+
+function sortearAmigo(){
+    if (nomesAmigos.length > 0){
+        let indice = parseInt(Math.random() * nomesAmigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = nomesAmigos[indice];
+    } else {
+        alert('Adicione amigos antes de sortear.');
+    }
+}
